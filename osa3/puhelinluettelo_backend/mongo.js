55ade659fd3d3ba98ea6def873123846ec27a65a
nemaaho <nemaaho@jyu.fi>
2024-10-28 14:16:19 +0200

    3.19, 3.20, 3.22

--- osa3/puhelinluettelo_backend/mongo.js
+++ osa3/puhelinluettelo_backend/mongo.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://nema:${password}@cluster0.lvwrb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://nema:${password}@cluster0.lvwrb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -23,27 +23,27 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     required: true,
   },
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
   name: process.argv[3],
   number: process.argv[4],
-});
+})
 
 if (process.argv.length === 5)
-  person.save().then((result) => {
-    console.log("added", person.name, "number", person.number, "to phonebook");
-    mongoose.connection.close();
-  });
+  person.save().then(() => {
+    console.log('added', person.name, 'number', person.number, 'to phonebook')
+    mongoose.connection.close()
+  })
 
 if (process.argv.length === 3) {
   Person.find({}).then((result) => {
-    console.log("phonebook:");
+    console.log('phonebook:')
     result.forEach((person) => {
-      console.log(person.name, person.number);
-    });
-    mongoose.connection.close();
-  });
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
 }
